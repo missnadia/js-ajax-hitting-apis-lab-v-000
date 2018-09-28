56bfc94c875692e2ab1d341226cc05d9bbf954a9
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-28 20:48:42 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -2,4 +2,10 @@ function getRepositories() {
   const name = document.getElementById('username').value;
   const uri = 'https://api.github.com' + '/users/' + name + '/repos';
   const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayRepositories);
+  xhr.open('GET', uri);
+  xhr.send();
+  return false;
 }
+
+function 
