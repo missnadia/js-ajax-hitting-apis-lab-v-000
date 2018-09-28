f5ed677cfec30203a3617dea142998780e0a9a9c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-28 20:57:13 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -12,19 +12,17 @@ function displayRepositories() {
   const repos = JSON.parse(this.responseText);
   const repoList =
     '<ul>' +
-    repos
-      .map(repo => {
-        const dataUsername = 'data-username="' + repo.owner.login + '"';
-        const dataRepoName = 'data-repository="' + repo.name + '"';
-        return `
-          <li>
-            <h2>${repo.name}</h2>
-            <a href="${repo.html_url}">${repo.html_url}</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-          </li>`;
-      })
-      .join('') +
+    repos.map(repo => {
+      const dataUsername = 'data-username="' + repo.owner.login + '"';
+      const dataRepoName = 'data-repository="' + repo.name + '"';
+      return `
+        <li>
+          <h2>${repo.name}</h2>
+          <a href="${repo.html_url}">${repo.html_url}</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+        </li>`;
+      }).join('') +
     '</ul>';
   document.getElementById('repositories').innerHTML = repoList;
 }
