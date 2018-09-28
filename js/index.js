85911de1ed4bc9d426786a6561acdba6ac332b44
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-28 21:13:10 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -16,13 +16,13 @@ function displayRepositories() {
         const dataUsername = 'data-username="' + repo.owner.login + '"';
         const dataRepoName = 'data-repository="' + repo.name + '"';
         return `
-          <li>
-            <h2>${repo.name}</h2>
-            <a href="${repo.html_url}">${repo.html_url}</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-          </li>`;
-      }).join('') + '</ul>';
+        <li>
+          <h2>${repo.name}</h2>
+          <a href="${repo.html_url}">${repo.html_url}</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+        </li>`;
+    }).join('') + '</ul>';
   document.getElementById('repositories').innerHTML = repoList;
 }
 
