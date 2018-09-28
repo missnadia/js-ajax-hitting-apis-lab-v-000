5680c8bc0303bb3743bd4fac988a0627092150df
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-28 21:16:12 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -34,10 +34,8 @@ function getCommits(x) {
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + 
-        ')</h3>' + commit.commit.message + '</li>'
+    .map(commit =>
+        '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>'
     ).join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
 }
