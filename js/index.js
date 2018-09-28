3a92298c591f815241a8c8083e1eb25fc0d3bdc2
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-28 21:17:20 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -33,7 +33,8 @@ function getCommits(x) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits.map(commit =>
+  const commitsList = `<ul>${commits.map(
+    commit =>
       '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>'
     ).join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
