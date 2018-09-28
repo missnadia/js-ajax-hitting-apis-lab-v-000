6d92ce59b77a5a5bf2e5d34142e684ed312eaa46
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-28 21:12:41 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -22,8 +22,7 @@ function displayRepositories() {
             <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
             <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
           </li>`;
-      })
-      .join('') + '</ul>';
+      }).join('') + '</ul>';
   document.getElementById('repositories').innerHTML = repoList;
 }
 
